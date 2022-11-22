43b2f0aaac5450214e87409376fea003be3f9e9b
Raj141020 <rajnagwanshi1410@gmail.com>
2022-11-22 22:15:52 +0530

    Project-2

--- routes/route.js
+++ routes/route.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const college = require('../Controller/collegeController')
+
 const intern = require('../Controller/internController')
 
 
-router.post('/functionup/colleges',college.creatcollege)
+router.post('/functionup/colleges',college.creatcollege) // Creating College Data
 
-router.post('/functionup/interns',intern.createIntern)
+router.post('/functionup/interns',intern.createIntern) // Creating Interns Data
 
-router.get('/functionup/collegeDetails',college.getlistofstudents)
-
+router.get('/functionup/collegeDetails',college.getlistofstudents) // Getting List of all Interns
 
 
 module.exports = router;
