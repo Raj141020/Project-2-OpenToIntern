8380934cc291547829857f144299268c3f380717
Raj141020 <rajnagwanshi1410@gmail.com>
2022-11-23 16:53:06 +0530

    Project-2

--- Models/collegeModel.js
+++ Models/collegeModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 const collegeModel = new mongoose.Schema(
     {
-        name: { type: String, require: true, unique: true, trim: true, lowercase: true},
+        name: { type: String, require: true, unique: true, lowercase: true},
         fullName: { type: String, require: true, trim: true },
-        logoLink: { type: String, require: true, trim: true },
+        logoLink: { type: String, require: true},
         isDeleted: { type: Boolean, default: false }
 
     }, { timestamps: true })
--- Models/internModel.js
+++ Models/internModel.js
@@ -4,7 +4,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 const internModel = new mongoose.Schema(
     {
         name: { type: String, require: true, trim: true },
-        email: { type: String, require: true, unique: true, trim: true, lowercase: true },
+        email: { type: String, require: true, unique: true, lowercase: true },
         mobile: { type: String, require: true, unique: true },
         collegeId: { type: ObjectId, ref: 'college' },
         isDeleted: { type: Boolean, default: false }
